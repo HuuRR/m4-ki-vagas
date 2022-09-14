9da78f3c7795ca7c278ff0ea53fc1c8dfcb64801
Kefren <kefrenkennedyav@gmail.com>
2022-09-14 16:55:37 -0300

    fix: same done as in the last commit but in updateUser.service.

--- src/services/users/updateUser.service.ts
+++ src/services/users/updateUser.service.ts
@@ -12,6 +12,23 @@ const updateUserService = async ({
   password,
   skills,
 }: IUpdateUser) => {
+
+  if(!cpf){
+    throw new AppError("This cpf is invalid.", 400)
+}
+
+  if(!email){
+    throw new AppError("This email is invalid.", 400)
+}
+
+if(!password){
+    throw new AppError("This password is invalid.", 400)
+}
+
+if(!name){
+    throw new AppError("This name is invalid.", 400)
+}
+
   const usersRepository = AppDataSource.getRepository(User);
 
   const user = await usersRepository.findOne({ where: { id } });

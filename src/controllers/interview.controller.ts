63ec9b3b55c60cdb497e4757cba2aaa8c5a33ae3
Kefren <kefrenkennedyav@gmail.com>
2022-09-14 18:09:54 -0300

    fix: geral fixes simmilar to the others.

--- src/controllers/interview.controller.ts
+++ src/controllers/interview.controller.ts
@@ -4,42 +4,121 @@ import listInterviewByUserService from "../services/interview/listInterviewsByUs
 import listInterviewByVacancyService from "../services/interview/listInterviewByVacancy.service";
 import editIterviewService from "../services/interview/editIterview.service";
 import deleteIterviewService from "../services/interview/deleteIterview.service";
+import AppDataSource from "../data-source";
+import { Vacancies } from "../entities/vacancies.entity";
+import { AppError } from "../errors/AppError";
+import { Interviews } from "../entities/interviews.entity";
 
-
-export const createInterviewController = async (request: Request, response: Response) => {
+export const createInterviewController = async (
+  request: Request,
+  response: Response
+) => {
   const { hour, date, userId, vacancyId } = request.body;
 
-  const newIterview = await createIterviewService({ hour, date, userId, vacancyId });
+  const vacancyRepository = AppDataSource.getRepository(Vacancies);
+
+  const thisvacancy = await vacancyRepository.findOne({
+    where: { id: vacancyId },
+  });
+
+  if (!thisvacancy) {
+    throw new AppError("Data not found.", 404);
+  }
+
+  const newIterview = await createIterviewService({
+    hour,
+    date,
+    userId,
+    vacancyId,
+  });
 
   return response.status(201).json(newIterview);
 };
 
-export const listInterviewByUserController = async (request: Request, response: Response) => {
+export const listInterviewByUserController = async (
+  request: Request,
+  response: Response
+) => {
   const { id } = request.params;
+
+  const interviewRepository = AppDataSource.getRepository(Interviews);
+
+  const thisInterview = await interviewRepository.findOne({
+    where: { id: id },
+  });
+
+  if (!thisInterview) {
+    throw new AppError("Data not found.", 404);
+  }
 
   const listInterviews = await listInterviewByUserService(id);
   return response.status(200).json(listInterviews);
 };
 
-export const listIterviewByVacancyController = async (request: Request, response: Response) => {
+export const listIterviewByVacancyController = async (
+  request: Request,
+  response: Response
+) => {
   const { id } = request.params;
+
+  const interviewRepository = AppDataSource.getRepository(Interviews);
+
+  const thisInterview = await interviewRepository.findOne({
+    where: { id: id },
+  });
+
+  if (!thisInterview) {
+    throw new AppError("Data not found.", 404);
+  }
 
   const listInterviews = await listInterviewByVacancyService(id);
   return response.status(200).json(listInterviews);
 };
 
-export const editIterviewController = async (request: Request, response: Response) => {
+export const editIterviewController = async (
+  request: Request,
+  response: Response
+) => {
   const { id } = request.params;
+
+  const interviewRepository = AppDataSource.getRepository(Interviews);
+
+  const thisInterview = await interviewRepository.findOne({
+    where: { id: id },
+  });
+
+  if (!thisInterview) {
+    throw new AppError("Data not found.", 404);
+  }
 
   const { hour, date, isOver, feedback } = request.body;
 
-  const interview = await editIterviewService({ id, hour, date, isOver, feedback });
+  const interview = await editIterviewService({
+    id,
+    hour,
+    date,
+    isOver,
+    feedback,
+  });
 
   return response.status(200).json(interview);
 };
 
-export const deleteIterviewController = async (request: Request, response: Response) => {
+export const deleteIterviewController = async (
+  request: Request,
+  response: Response
+) => {
   const { id } = request.params;
+
+  const interviewRepository = AppDataSource.getRepository(Interviews);
+
+  const thisInterview = await interviewRepository.findOne({
+    where: { id: id },
+  });
+
+  if (!thisInterview) {
+    throw new AppError("Data not found.", 404);
+  }
 
   const message = await deleteIterviewService(id);
 
--- src/services/interview/createInterview.service.ts
+++ src/services/interview/createInterview.service.ts
@@ -6,19 +6,30 @@ import { Interviews } from "../../entities/interviews.entity";
 import { Vacancies } from "../../entities/vacancies.entity";
 import { instanceToPlain } from "class-transformer";
 
+const createIterviewService = async ({
+  hour,
+  date,
+  userId,
+  vacancyId,
+}: IInterviewRequest) => {
+  if (!hour || !date || !userId || !vacancyId) {
+    throw new AppError(
+      "All fields must be filled. (hour, date, userId, vacancyId).",
+      404
+    );
+  }
 
-const createIterviewService = async ({ hour, date, userId, vacancyId}: IInterviewRequest) => {
   const interviewRepository = AppDataSource.getRepository(Interviews);
 
   const usersRepository = AppDataSource.getRepository(User);
-  
+
   const vacancyRepository = AppDataSource.getRepository(Vacancies);
 
-  const user = await usersRepository.findOne({ where: { id: userId}});
+  const user = await usersRepository.findOne({ where: { id: userId } });
 
   if (!user) throw new AppError("User not found.", 404);
 
-  const vacancy = await vacancyRepository.findOne({where: { id: vacancyId}});
+  const vacancy = await vacancyRepository.findOne({ where: { id: vacancyId } });
 
   if (!vacancy) throw new AppError("Vacancy not found.", 404);
 
@@ -27,18 +38,18 @@ const createIterviewService = async ({ hour, date, userId, vacancyId}: IIntervie
   let days = dateFormat.getDay();
 
   if (days === 0 || days === 6) {
-    throw new AppError("Invalid Date.");
-  };
+    throw new AppError("Invalid Date. (must be like 2012/10/12 for example).", 400);
+  }
 
   if (parseInt(hour) > 18 || parseInt(hour) < 8) {
-    throw new AppError("Invalid hour.");
-  };
+    throw new AppError("Invalid hour. (must be like 10:00 for example).", 400);
+  }
 
   const newInterview = interviewRepository.create({
     date,
     hour,
     user,
-    vacancy
+    vacancy,
   });
 
   await interviewRepository.save(newInterview);
